5adf06eb884dd5286bc107217e4a49fcac55719b
suyeon801 <suyeon801@duksung.ac.kr>
2023-01-04 14:01:56 +0900

    main.xml

---
+++ ScoreBoard/app/src/main/java/com/example/scoreboard/Menu.java
@@ -0,0 +1,14 @@
+package com.example.scoreboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
