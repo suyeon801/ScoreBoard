4944919d776894f950c11f45fc9504728fbaa59b
suyeon801 <suyeon801@duksung.ac.kr>
2023-01-15 18:18:28 +0900

    menu -> UserList

--- ScoreBoard/app/src/main/java/com/example/scoreboard/Menu.java
+++ ScoreBoard/app/src/main/java/com/example/scoreboard/Menu.java
@@ -2,7 +2,10 @@ package com.example.scoreboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button userList = (Button) findViewById(R.id.user_list);
+        userList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent userIntent = new Intent(getApplicationContext(), UserList.class);
+                startActivity(userIntent);
+            }
+        });
     }
 }
