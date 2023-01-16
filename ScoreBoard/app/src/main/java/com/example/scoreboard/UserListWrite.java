ae27c718add3e1db627ea90e715c10a99957c125
suyeon801 <suyeon801@duksung.ac.kr>
2023-01-16 14:36:51 +0900

    add UserList

--- ScoreBoard/app/src/main/java/com/example/scoreboard/UserListWrite.java
+++ ScoreBoard/app/src/main/java/com/example/scoreboard/UserListWrite.java
@@ -2,7 +2,10 @@ package com.example.scoreboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserListWrite extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class UserListWrite extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list_write);
+
+        Button addButton = (Button) findViewById(R.id.addbutton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addIntent = new Intent(getApplicationContext(), UserList.class);
+                startActivity(addIntent);
+            }
+        });
     }
 }
