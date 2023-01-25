21b97be9f1e774cd2cd13aa16955622ed270261c
suyeon801 <suyeon801@duksung.ac.kr>
2023-01-25 21:00:40 +0900

    add intent

--- ScoreBoard/app/src/main/java/com/example/scoreboard/UserList.java
+++ ScoreBoard/app/src/main/java/com/example/scoreboard/UserList.java
@@ -1,28 +1,36 @@
 package com.example.scoreboard;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class UserList extends AppCompatActivity {
     RecyclerView recyclerView;
+    Button addButton;
+    UserAdapter userAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
 
-        Button addButton = (Button) findViewById(R.id.add);
+        //add user button
+        addButton = (Button) findViewById(R.id.add);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent addIntent = new Intent(getApplicationContext(), UserListWrite.class);
-                startActivity(addIntent);
+                startActivityResult.launch(addIntent);
             }
         });
 
@@ -30,7 +38,7 @@ public class UserList extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(layoutManager);
 
-        UserAdapter userAdapter = new UserAdapter(getApplicationContext());
+        userAdapter = new UserAdapter(getApplicationContext());
 
         //데이터 설정
         userAdapter.addItem(new UserItem("#1", "김수연", "2000-11-08", "Female"));
@@ -41,4 +49,15 @@ public class UserList extends AppCompatActivity {
         recyclerView.setAdapter(userAdapter);
         userAdapter.notifyDataSetChanged();
     }
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        //다른 액티비티를 다녀와서 실행할 작업 작성
+                        userAdapter.notifyDataSetChanged();
+                    }
+                }
+            });
 }
--- ScoreBoard/app/src/main/java/com/example/scoreboard/UserListWrite.java
+++ ScoreBoard/app/src/main/java/com/example/scoreboard/UserListWrite.java
@@ -1,25 +1,45 @@
 package com.example.scoreboard;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class UserListWrite extends AppCompatActivity {
+
+    EditText u_name_write;
+    EditText u_birth_write;
+    EditText u_gender_write;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list_write);
 
+        //editText findID
+        u_name_write = (EditText) findViewById(R.id.u_name_write);
+        u_birth_write = (EditText) findViewById(R.id.u_birth_write);
+        u_gender_write = (EditText) findViewById(R.id.u_gender_write);
+
+        //add user
         Button addButton = (Button) findViewById(R.id.addbutton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent addIntent = new Intent(getApplicationContext(), UserList.class);
-                startActivity(addIntent);
+
+                addIntent.putExtra("user_name", u_name_write.getText().toString());
+                addIntent.putExtra("user_birth", u_birth_write.getText().toString());
+                addIntent.putExtra("user_gender", u_gender_write.getText().toString());
+
+                addIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                setResult(Activity.RESULT_OK, addIntent);
+                //startActivity(addIntent);
+                finish();
             }
         });
     }
