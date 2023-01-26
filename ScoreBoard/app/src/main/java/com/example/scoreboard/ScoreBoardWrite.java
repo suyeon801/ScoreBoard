c4710d4992127325f78dabdb0fcfcca156d23950
suyeon801 <suyeon801@duksung.ac.kr>
2023-01-26 14:19:34 +0900

    add spinner

--- ScoreBoard/app/src/main/java/com/example/scoreboard/ScoreBoardWrite.java
+++ ScoreBoard/app/src/main/java/com/example/scoreboard/ScoreBoardWrite.java
@@ -9,30 +9,30 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
 public class ScoreBoardWrite extends AppCompatActivity {
     TextView gameSpinnerResult;
     Spinner gameSpinner;
     ArrayAdapter<String> spinnerAdapter;
     //GameAdapter spinnerAdapter;
 
-    ArrayList<GameItem> gameItemArrayList = new ArrayList<GameItem>();
+    String[] gameItemArrayList = {"Baseball", "Socceer", "Basketball"};
+
+    //ArrayList<GameItem> gameItemArrayList = new ArrayList<GameItem>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_board_write);
 
-        gameSpinnerResult.findViewById(R.id.gameSpinnerResult);
-        gameSpinner.findViewById(R.id.gameSpinner);
+        gameSpinnerResult = (TextView) findViewById(R.id.gameSpinnerResult);
+        gameSpinner = (Spinner) findViewById(R.id.gameSpinner);
 
         spinnerAdapter =new ArrayAdapter(this, android.R.layout.simple_spinner_item, gameItemArrayList);
         gameSpinner.setAdapter(spinnerAdapter);
         gameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                gameSpinnerResult.setText((CharSequence) gameItemArrayList.get(position));
+                gameSpinnerResult.setText(gameItemArrayList[position]);
             }
 
             @Override
