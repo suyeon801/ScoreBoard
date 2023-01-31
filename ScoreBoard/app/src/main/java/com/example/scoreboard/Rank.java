4d9f8a8cfb8ce609d01bc47aea56b21f34d347a6
suyeon801 <suyeon801@duksung.ac.kr>
2023-01-31 14:12:23 +0900

    add Rank_recyclerview

--- ScoreBoard/app/src/main/java/com/example/scoreboard/Rank.java
+++ ScoreBoard/app/src/main/java/com/example/scoreboard/Rank.java
@@ -1,14 +1,30 @@
 package com.example.scoreboard;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class Rank extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TotalAdapter totalAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank);
+
+        //recyclerView
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView_totalRank);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        totalAdapter = new TotalAdapter(getApplicationContext());
+
+        totalAdapter.addItem(new TotalItem(4, "D player", 10));
+
+        recyclerView.setAdapter(totalAdapter);
     }
 }
